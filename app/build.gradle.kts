f406cd80e568a5e25570532a5f8aa620a4357061
sefa <erzan@code.id>
2026-05-18 03:22:55 +0700

    chore: release v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yuwanadev.mdm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
