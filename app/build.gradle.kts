0086a086807c96be73c2da9672534536576e7f05
sefa <erzan@code.id>
2026-05-19 17:14:22 +0700

    chore: release v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yuwanadev.mdm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     // Load local.properties if it exists
